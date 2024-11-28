9ae5260dd1b8d75d4cd1c5408dbce35f79fa3f00
zeleski79 <zeleski@hotmail.com>
2024-11-28 16:43:19 +0100

    End of section 12

---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities;
+
+public class ShoppingCart
+{
+    public required string Id { get; set; }
+    public List<CartItem> Items { get; set; } = [];
+    // public int? DeliveryMethodId { get; set; }
+    // public string? ClientSecret { get; set; }
+    // public string? PaymentIntentId { get; set; }
+    // public AppCoupon? Coupon { get; set; }
+}
